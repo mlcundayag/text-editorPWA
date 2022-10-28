5ad63603b4f6e4c70b289c3e8b0d05cef02970dd
mlcundayag <mlcundayag@gmail.com>
2022-10-27 23:08:55 -1000

    set up logic to add content to db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+import { request } from 'express';
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -12,12 +13,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  
-  console.error('putDb not implemented')};
+  console.log('Updating database');
+  if(err){
+    console.error('putDb not implemented')
+  } else {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ jate: content })
+  const result = await request;
+  console.log("Success", result)
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  if(err){
+    console.error('getDb not implemented');
+  } else {
+    const jateDB = await openDB("jate", 1);
+    const tx = 
+  }
+} 
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+//configure workbox plugins for a service worker and manifest file.
 module.exports = () => {
   return {
     mode: 'development',
@@ -42,7 +42,7 @@ module.exports = () => {
       }),
     ],
     
-    // TODO: Add CSS loaders and babel to webpack.
+    // set up CSS loaders and babel to webpack.
     module: {
       rules: [
         {
