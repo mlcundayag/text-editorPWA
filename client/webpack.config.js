700073a3b0714800eb4c25eddb36e0156cc5a182
mlcundayag <mlcundayag@gmail.com>
2022-10-28 00:01:05 -1000

    for heroku deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+//Add and configure workbox plugins for a service worker and manifest file.
 module.exports = () => {
   return {
     mode: 'development',
@@ -45,7 +43,7 @@ module.exports = () => {
         ]
       })
     ],
-
+//Add CSS loaders and babel to webpack.
     module: {
       rules: [
         {
