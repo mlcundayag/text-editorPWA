8caf67b89efb85e4aa494b4f2919fea51f46fe48
mlcundayag <mlcundayag@gmail.com>
2022-10-27 23:21:59 -1000

    update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,31 +15,22 @@ const initdb = async () =>
 
 //Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Updating database');
-  if(err){
-    console.error('putDb not implemented')
-  } else {
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
   const request = store.put({ jate: content })
   const result = await request;
-  console.log("Success", result)
-  }
+  result ? console.log("succes", result) : console.error('putDb not implemented');
 };
 
 //Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  if(err){
-    console.error('getDb not implemented');
-  } else {
     const jateDB = await openDB("jate", 1);
     const tx = jateDB.transaction("jate", "readonly");
     const store = tx.objectStore("jate");
     const request = store.getAll();
     const result = await request;
-    console.log(result)
-  }
+    result ? console.log(result) : console.error('getDb not implemented');
 } 
 
 initdb();
